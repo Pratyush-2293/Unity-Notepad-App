161f5c4e4fe2e29047807d611bfcd17c9539c979
Pratyush-2293 <pratyush12gawali@gmail.com>
2024-09-23 15:50:48 +0530

    Created the UI layout

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour
+{
+    public GameObject theText = null;
+
+    public void ClearText()
+    {
+        theText.GetComponent<InputField>().text = "";
+    }
+}
